8bdf672caef58f01c67c971d07433afaf3ff8d4c
AIToala <ai_toala@outlook.com>
2024-07-13 16:49:51 -0500

    chore: Update configuration file formatting

--- config/config.js
+++ config/config.js
@@ -1,25 +1,25 @@
 /* eslint-disable no-undef */
-require('dotenv').config();
+require("dotenv").config();
 module.exports = {
-	development: {
-		username: process.env.DEV_USERNAME,
-		password: process.env.DEV_PASSWORD,
-		database: process.env.DEV_DATABASE_DEVELOPMENT,
-		host: process.env.DEV_HOST,
-		dialect: process.env.DEV_DIALECT,
-	},
-	test: {
-		username: process.env.TEST_USERNAME,
-		password: process.env.TEST_PASSWORD,
-		database: process.env.TEST_DATABASE_TEST,
-		host: process.env.TEST_HOST,
-		dialect: process.env.TEST_DIALECT,
-	},
-	production: {
-		username: process.env.DB_USERNAME,
-		password: process.env.DB_PASSWORD,
-		database: process.env.DB_DATABASE_PRODUCTION,
-		host: process.env.DB_HOST,
-		dialect: process.env.DB_DIALECT,
-	},
+  development: {
+    username: process.env.DEV_USERNAME,
+    password: process.env.DEV_PASSWORD,
+    database: process.env.DEV_DATABASE_DEVELOPMENT,
+    host: process.env.DEV_HOST,
+    dialect: process.env.DEV_DIALECT,
+  },
+  test: {
+    username: process.env.TEST_USERNAME,
+    password: process.env.TEST_PASSWORD,
+    database: process.env.TEST_DATABASE_TEST,
+    host: process.env.TEST_HOST,
+    dialect: process.env.TEST_DIALECT,
+  },
+  production: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE_PRODUCTION,
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT,
+  },
 };
